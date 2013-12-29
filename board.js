abb0583686cde9181a714aee773e24dcd32a7b54
Chris LaRose <cjlarose@gmail.com>
2013-12-29 16:13:05 -0700

    Implemented count_liberties

--- board.js
+++ board.js
@@ -21,11 +21,49 @@ Board.prototype.create_board = function(size) {
 Board.prototype.play = function(i, j) {
     console.log("Played at " + i + ", " + j);   
     this.board[i][j] = this.current_color;
-    $(this).trigger({
-        type: "update",
-        color: this.current_color,
-        i: i,
-        j: j
-    });
+    $(this).trigger("update");
     this.current_color = this.current_color == Board.BLACK ? Board.WHITE : Board.BLACK;
+};
+
+Board.prototype.get_adjacent_intersections = function(i , j) {
+    var neighbors = []; 
+    if (i > 0)
+        neighbors.push([i - 1, j]);
+    if (j < this.size - 1)
+        neighbors.push([i, j + 1]);
+    if (i < this.size - 1)
+        neighbors.push([i + 1, j]);
+    if (j > 0)
+        neighbors.push([i, j - 1]);
+    return neighbors;
+};
+
+Board.prototype.count_liberties = function(i, j) {
+
+    var color = this.board[i][j];
+    if (color == Board.EMPTY)
+        return null;
+
+    var visited = {};
+    var queue = [[i, j]];
+    var count = 0;
+
+    while (queue.length > 0) {
+        var node = queue.pop();
+        if (visited[node])
+            continue;
+
+        var neighbors = this.get_adjacent_intersections(node[0], node[1]);
+        for (var n = 0; n < neighbors.length; n++) {
+            var state = this.board[neighbors[n][0]][neighbors[n][1]];
+            if (state == Board.EMPTY)
+                count++;
+            if (state == color)
+                queue.push([neighbors[n][0], neighbors[n][1]]);
+        }
+
+        visited[node] = true;
+    }
+
+    return count;
 };
