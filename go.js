e75100c7a2cf328c61535fde68bc77e370b75d26
Chris LaRose <cjlarose@gmail.com>
2013-12-29 15:28:55 -0700

    Moved board model into separate js file

--- go.js
+++ go.js
@@ -48,38 +48,6 @@ var BoardView = React.createClass({
     }
 });
 
-var Board = function(size) {
-    this.current_color = Board.BLACK;
-    this.size = size;
-    this.board = this.create_board(size);
-};
-
-Board.EMPTY = 0;
-Board.BLACK = 1;
-Board.WHITE = 2;
-
-Board.prototype.create_board = function(size) {
-    var m = [];
-    for (var i = 0; i < size; i++) {
-        m[i] = [];
-        for (var j = 0; j < size; j++)
-            m[i][j] = 0;
-    }
-    return m;
-};
-
-Board.prototype.play = function(i, j) {
-    console.log("Played at " + i + ", " + j);   
-    this.board[i][j] = this.current_color;
-    $(this).trigger({
-        type: "update",
-        color: this.current_color,
-        i: i,
-        j: j
-    });
-    this.current_color = this.current_color == Board.BLACK ? Board.WHITE : Board.BLACK;
-};
-
 var board = new Board(13);
 
 React.renderComponent(
